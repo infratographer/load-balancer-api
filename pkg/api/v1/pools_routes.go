5cd0f5490441440646e2b4650311c9f5df633c4f
Katie Hegarty <khegarty@equinix.com>
2023-02-21 08:35:41 -0500

    Update pool routes with tenant id (#45)
    Signed-off-by: Katie Hegarty <khegarty@equinix.com>

--- pkg/api/v1/pools_routes.go
+++ pkg/api/v1/pools_routes.go
@@ -4,11 +4,11 @@ import "github.com/labstack/echo/v4"
 
 // addPoolsRoutes adds the routes for the pools API
 func (r *Router) addPoolsRoutes(g *echo.Group) {
-	g.GET("/pools", r.poolsList)
+	g.GET("/tenant/:tenant_id/pools", r.poolsList)
 	g.GET("/pools/:pool_id", r.poolsGet)
 
-	g.POST("/pools", r.poolCreate)
+	g.POST("/tenant/:tenant_id/pools", r.poolCreate)
 
-	g.DELETE("/pools", r.poolDelete)
+	g.DELETE("/tenant/:tenant_id/pools", r.poolDelete)
 	g.DELETE("/pools/:pool_id", r.poolDelete)
 }
