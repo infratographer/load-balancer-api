ead52a02651b043add89de721905f47a00e71e9b
Mike Mason <mimason@equinix.com>
2023-06-27 14:07:34 +0000

    add loadbalancer permission checks
    Signed-off-by: Mike Mason <mimason@equinix.com>

---
+++ internal/graphapi/permissions.go
@@ -0,0 +1,8 @@
+package graphapi
+
+const (
+	actionLoadBalancerCreate = "loadbalancer_create"
+	actionLoadBalancerUpdate = "loadbalancer_update"
+	actionLoadBalancerDelete = "loadbalancer_delete"
+	actionLoadBalancerGet    = "loadbalancer_get"
+)
