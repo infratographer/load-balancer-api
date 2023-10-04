9f449ddc7f546e1db56d4e4919d740ee4c5c6e53
sthwang-metal <126002920+sthwang-metal@users.noreply.github.com>
2023-10-04 07:53:00 -0700

    Add Resolver Validations (#243)
    * add more checks to resolvers
    
    Signed-off-by: Stephen Hwang <126002920+sthwang-metal@users.noreply.github.com>
    
    * add tests
    
    Signed-off-by: Stephen Hwang <126002920+sthwang-metal@users.noreply.github.com>
    
    * add more tests
    
    Signed-off-by: Stephen Hwang <126002920+sthwang-metal@users.noreply.github.com>
    
    * update permission checks
    
    Signed-off-by: Stephen Hwang <126002920+sthwang-metal@users.noreply.github.com>
    
    * update query method
    
    Signed-off-by: Stephen Hwang <126002920+sthwang-metal@users.noreply.github.com>
    
    * remove tests
    
    Signed-off-by: Stephen Hwang <126002920+sthwang-metal@users.noreply.github.com>
    
    * update error
    
    Signed-off-by: Stephen Hwang <126002920+sthwang-metal@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Stephen Hwang <126002920+sthwang-metal@users.noreply.github.com>

--- internal/graphapi/errors.go
+++ internal/graphapi/errors.go
@@ -7,3 +7,9 @@ var ErrPortNumberInUse = errors.New("port number already in use")
 
 // ErrRestrictedPortNumber is returned when a port number is restricted.
 var ErrRestrictedPortNumber = errors.New("port number restricted")
+
+// ErrPortNotFound is returned when one or more ports are not found
+var ErrPortNotFound = errors.New("one or more ports not found")
+
+// ErrPoolNotFound is returned when one or more pools are not found
+var ErrPoolNotFound = errors.New("one or more pools not found")
