99120425f77a92d1b11583e7f04e52c001f4c9bb
Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>
2024-02-08 07:11:50 -0500

    Fix migrations
    Signed-off-by: Tyler Auerbeck <tylerauerbeck@users.noreply.github.com>

--- db/migrations/20240207222332_softdelete.sql
+++
@@ -1,7 +0,0 @@
--- +goose Up
--- modify "load_balancers" table
-ALTER TABLE "load_balancers" ADD COLUMN "deleted_at" timestamptz NULL, ADD COLUMN "deleted_by" character varying NULL;
-
--- +goose Down
--- reverse: modify "load_balancers" table
-ALTER TABLE "load_balancers" DROP COLUMN "deleted_by", DROP COLUMN "deleted_at";
